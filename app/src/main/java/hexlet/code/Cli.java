48a2b0d99638ff2510d9826fb526aef2a3ffe9bf
utamuradov <utamuradov@arrival.com>
2022-07-12 14:57:47 +0500

    - Добавил незакоммиченные gradle файлы - Реализовал Cli класс

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,17 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void welcome() {
+
+        Scanner cliScanner = new Scanner(System.in);
+
+        System.out.print("May I have your name? ");
+
+        String name = cliScanner.nextLine();
+
+        System.out.printf("Hello, %s!", name);
+    }
+}
